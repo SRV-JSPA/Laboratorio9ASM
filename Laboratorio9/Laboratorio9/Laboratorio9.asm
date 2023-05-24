6baa991fa00b6f4093848fc78bcd75ec3642401a
nancygmm <99931623+nancygmm@users.noreply.github.com>
2023-05-23 23:02:20 -0600

    Comentarios

--- Laboratorio9/Laboratorio9/Laboratorio9.asm
+++ Laboratorio9/Laboratorio9/Laboratorio9.asm
@@ -1,6 +1,11 @@
 ;Universidad del Valle de Guatemala
 ;Nancy Mazariegos 22513
 ;Santiago Pereira 22318
+;Organización de computadoras y Assembler
 ;Roger Díaz
 ;Descripcion: Laboratorio 9
 ;24/05/2023
+
+.386
+.model flat, stdcall, c
+.stack 4096
